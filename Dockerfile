2da005f3a43d4acc7bacc69052d058304223ccce
Dion Häfner <dimh@dhigroup.com>
2018-08-09 13:41:54 +0200

    correcter globbing behavior

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ RUN apt-get update && apt-get install -y \
         'make' \
         'curl' \
         'gdal-bin' \
+        'python' \
         'libtiff-dev' \
         'libjpeg-dev' \
         'libxml2-dev' \
--- run_lasrc.sh
+++ run_lasrc.sh
@@ -19,7 +19,8 @@ mkdir -p $WORKDIR
 cd $WORKDIR
 
 # only make files with the correct scene ID visible
-for f in "$INDIR/${SCENE_ID}_*.tif" "$INDIR/${SCENE_ID}_*.TIF"; do
+TIF_PATTERNS="$INDIR/${SCENE_ID}_*.tif $INDIR/${SCENE_ID}_*.TIF"
+for f in $TIF_PATTERNS; do
     if $(gdalinfo $f | grep -o 'Block=.*x1\s'); then
         ln -s $f $WORKDIR/$(basename $f)
     else
@@ -35,7 +36,8 @@ do_lasrc.py --xml ${SCENE_ID}.xml --write-toa
 cloud_masking.py --xml ${SCENE_ID}.xml
 convert_espa_to_gtif --xml=${SCENE_ID}.xml --gtif=$SCENE_ID --del_src_files
 
-for f in "$WORKDIR/${SCENE_ID}_toa_*.tif" "$WORKDIR/${SCENE_ID}_sr_*.tif" "$WORKDIR/${SCENE_ID}_radsat_qa.tif" "$WORKDIR/${SCENE_ID}_cfmask*.tif" "$WORKDIR/${SCENE_ID}_MTL.txt"; do
+OUT_PATTERNS="$WORKDIR/${SCENE_ID}_toa_*.tif $WORKDIR/${SCENE_ID}_sr_*.tif $WORKDIR/${SCENE_ID}_radsat_qa.tif $WORKDIR/${SCENE_ID}_cfmask*.tif $WORKDIR/${SCENE_ID}_MTL.txt"
+for f in $OUT_PATTERNS; do
     mv $f $OUTDIR
 done
 
